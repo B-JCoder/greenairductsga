5a9daf4f98dc32baf74754ef9341850c097f76e2
Bilal Jadi <148371977+B-JCoder@users.noreply.github.com>
2025-07-31 11:52:46 -0700

    whatsapp button p solve

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,9 @@ export default function Home() {
       <GallerySection />
       <ContactForm />
       <FAQSection />
-      <Footer />
       <WhatsAppButton />
+      <Footer />
+      
     </main>
   )
 }
